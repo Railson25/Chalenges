7ff7f7665adad36bc60113bc2093e156a361d8c3
Railson25 <railsonsantiago123@gmail.com>
2022-08-08 22:17:30 -0300

    Fixing and ScreenHome

--- todoList/src/Home/index.tsx
+++ todoList/src/Home/index.tsx
@@ -2,7 +2,7 @@ import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 import LogoImg from '../assets/logo.png'
-import AddButton from '../assets/addButton.png'
+import ClipBoard from '../assets/Clipboard.png'
 
 export function Home() {
     return(
@@ -16,6 +16,23 @@ export function Home() {
                 <Text style={styles.headerTextTwo}>do</Text>
             </View>
             
+            <View style={styles.content}>
+                <View style={styles.contentHeader}>
+                    <Text style={styles.contentHeaderTextOne}>Criadas</Text>
+                    <Text style={styles.contentHeaderTextTwo}>Concluidas</Text>
+                </View>
+                
+                <View style={styles.empityList}>
+                    <Image 
+                        source={ClipBoard }
+                        width={56}
+                        height={56}
+                    />
+                    <Text style={styles.empityListTextOne}>Você ainda não tem tarefas cadastradas </Text>
+                    <Text style={styles.empityListTextTwo}>Crie tarefas e organize seus itens a fazer</Text>
+                </View>
+            </View>
+
             <View style={styles.form}>
                 <TextInput 
                     style={styles.input}
@@ -25,10 +42,6 @@ export function Home() {
                 <TouchableOpacity style={styles.button}>
                     <Text style={styles.buttonText}>+</Text>
                 </TouchableOpacity>
-            </View>
-
-            <View style={styles.content}>
-                {/* <Text style={styles.contentText}>Testandoooaooasoidi</Text> */}
             </View>
         </View>
     )
--- todoList/src/Home/styles.ts
+++ todoList/src/Home/styles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        position: "relative"
     },
     header:{
         backgroundColor: '#0d0d0d',
@@ -58,14 +57,51 @@ export const styles = StyleSheet.create({
         color: 'white',
     },
     content:{
-       
-        
-        backgroundColor: '#1A1A1A',
-       
-        
+        backgroundColor: '#1A1A1A',   
+        flex: 1,
+        padding: 24
     },
-    contentText:{
-        
-        color:'white'
+    contentHeader:{
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        top: 55,
+    },
+    contentHeaderTextOne:{
+        color: '#4EA8DE',
+        fontWeight: '700',
+        fontSize: 14,
+        lineHeight: 16.94
+    },
+
+    contentHeaderTextTwo:{
+        color: '#8284FA',
+        fontWeight: '700',
+        fontSize: 14,
+        lineHeight: 16.94,
+        marginBottom: 20
+    },
+    empityList: {
+        marginTop: 68,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        paddingTop: 48,
+        paddingBottom: 48,
+        paddingLeft:20,
+        paddingRight: 20,
+    },
+    empityListTextOne:{
+        marginTop: 16,
+        fontSize: 16,
+        fontWeight: "700",
+        color: '#808080',
+        lineHeight: 19.6
+    },
+    empityListTextTwo:{
+        marginTop: 16,
+        fontSize: 16,
+        color: '#808080',
+        fontWeight: "400",
+        lineHeight: 19.6
     }
 })
